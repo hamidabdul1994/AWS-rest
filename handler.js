084d52ce91c7cc251edc85da707d10159b3a0636
Hamid Raza <hamidabdul1994@hotmail.com>
2017-03-09 15:23:36 +0530

    Get User data

--- handler.js
+++ handler.js
@@ -14,53 +14,60 @@ module.exports.hello = (event, context, callback) => {
     callback(null, response);
 };
 module.exports.getUser = (event, context, callback) => {
+  const params = {TableName: process.env.DYNAMODB_TABLE || "EngineerTable"};
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the todos.'));
+      return;
+    }
+    // create a response
     const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Get User data v1.0!',
-            event: event
-        })
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
     };
     callback(null, response);
+  });
 };
 module.exports.createUser = (event, context, callback) => {
-        const timestamp = new Date().getTime();
-        const data = JSON.parse(event.body);
-        console.log(data);
-        if (typeof data.text !== 'string') {
-            console.error('Validation Failed');
-            callback(new Error('Couldn\'t create the todo item.'));
-            return;
+  try {
+    const timestamp = new Date().getTime();
+    const data = ("string"===typeof event.body)?JSON.parse(event.body):event.body;
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE || "EngineerTable",
+        Item: {
+            id: uuid.v1(),
+            engineerId: data.engineerId,
+            engineerName: data.engineerName,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
         }
+    };
+    console.log("params:",params);
 
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE,
-            Item: {
-                id: uuid.v1(),
-                engineerId: data.engineerId,
-                engineerName: data.engineerName,
-                checked: false,
-                createdAt: timestamp,
-                updatedAt: timestamp,
+    // write the todo to the database
+    dynamoDb.put(params, (error, result) => {
+            // handle potential errors
+            if (error) {
+                console.error(error);
+                callback(new Error('Couldn\'t create the todo item.'));
+                return;
             }
-        };
 
-        // write the todo to the database
-        dynamoDb.put(params, (error, result) => {
-                // handle potential errors
-                if (error) {
-                    console.error(error);
-                    callback(new Error('Couldn\'t create the todo item.'));
-                    return;
-                }
+            // create a response
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(result.Item),
+            };
+            callback(null, response);
+        });
+  } catch (e) {
+    callback(null, e);
+  }
 
-                // create a response
-                const response = {
-                    statusCode: 200,
-                    body: JSON.stringify(result.Item),
-                };
-                callback(null, response);
-            });
+
         };
         module.exports.deleteUser = (event, context, callback) => {
             var body_data = ("string" === typeof event.body) ? JSON.parse(event.body) : event.body;
